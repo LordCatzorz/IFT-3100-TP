4050ce989053662da5dc945754df7f410829bbef
LordCatzorz <LordCatzorz@users.noreply.github.com>
2016-02-21 17:09:26 -0500

    Missing file?

--- src/Shapes.h
+++ src/Shapes.h
@@ -6,7 +6,17 @@ class Shapes
 public:
 	Shapes();
 	~Shapes();
+	ofMesh* createTetrahedron();
+	ofMesh* createHexahedron();
+	ofMesh* createOctahedron();
+	ofMesh* createDodecahedron();
+	ofMesh* createIcosahedron();
+
 	ofMesh* createCube();
+
+	ofMesh* createSphere();
+
+	
 private:
 
 };
