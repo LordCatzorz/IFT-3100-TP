5c443b8ce5ad4e160277ae9876d5209664f9b04d
LordCatzorz <LordCatzorz@users.noreply.github.com>
2016-02-21 21:29:10 -0500

    Correction pour compilation sous Windows.

--- src/GUI.h
+++ src/GUI.h
@@ -5,7 +5,9 @@
 
 #include "ofMain.h"
 #include "ofxGui.h"
+#ifdef __linux__
 #include "gtk/gtk.h"
+#endif
 #include <fstream>
 #include <iterator>
 #include <algorithm>
@@ -13,16 +15,16 @@
 class GUI
 {
 public:
-    GUI();
-    void Draw();
-    void AddImageOpenedListener(void (inCallBackFunction)(string));
+	GUI();
+	void Draw();
+	void AddImageOpenedListener(void (inCallBackFunction)(string));
 private:
-    ofxButton openFileBtn;
-    ofxPanel gui;
-    void openFileBtnCallback();
-    ofFileDialogResult requestUsrFile();
-    void saveFile(string path, std::ifstream & file);
-    void (*callBackFunction)(string);
+	ofxButton openFileBtn;
+	ofxPanel gui;
+	void openFileBtnCallback();
+	ofFileDialogResult requestUsrFile();
+	void saveFile(string path, std::ifstream & file);
+	void(*callBackFunction)(string);
 };
 
 #endif // GUI_H
