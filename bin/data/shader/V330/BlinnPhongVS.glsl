4e83ae9419b7b873a0832f50b2d37e7e53f7b276
LordCatzorz <LordCatzorz@users.noreply.github.com>
2016-03-11 18:30:06 -0500

    Added files

---
+++ bin/data/shader/V330/BlinnPhongVS.glsl
@@ -0,0 +1,29 @@
+// IFT3100H16 ~ BlinnPhongVS.glsl
+
+#version 330
+
+// attribut de sommet
+in vec4 position;
+in vec4 normal;
+
+// attributs en sortie
+out vec3 interpolationPosition;
+out vec3 interpolationNormal;
+
+// attributs uniformes
+uniform mat4x4 modelViewMatrix;
+uniform mat4x4 projectionMatrix;
+
+void main()
+{
+	mat4x4 normalMatrix = transpose(inverse(modelViewMatrix));
+
+	// transformation de la normale du sommet dans l'espace de vue
+	interpolationNormal = vec3(normalMatrix * normal);
+
+	// transformation de la position du sommet dans l'espace de vue
+	interpolationPosition = vec3(modelViewMatrix * position);
+
+	// transformation de la position du sommet par les matrices de modèle, vue et projection
+	gl_Position = projectionMatrix * modelViewMatrix * position;
+}
